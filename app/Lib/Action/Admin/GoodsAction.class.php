f369ca56dd7e2ab5f54739a57fc5c45dd99c43eb
15623779417 <15623779417@163.com>
2014-11-11 18:04:53 +0800

    20141111
    商品添加

--- app/Lib/Action/Admin/GoodsAction.class.php
+++ app/Lib/Action/Admin/GoodsAction.class.php
@@ -5,31 +5,52 @@
  * @author ChenJian
  *
  */
-class GoodsAction extends Action{
+class GoodsAction extends SimpleAction{
 	
 	function index()
 	{
-		
-		
+		$list = $this->_model->select();
+		$this->assign('list',$list);
 		$this->display();
 	}
 	
 	function add()
 	{
-		//商品品牌
-		$_brand_mod = D("GBrand");
-		$brand_list = $_brand_mod->select();
-		$this->assign('brand_list',$brand_list);
-		
 		if(IS_POST)
 		{
-			pr($_POST);	
+			$data = $this->_model->create();
+			$data['add_time'] = time();			
+			
+			if(!$data || ($id = $this->_model->add($data)) === false )
+			{
+				$this->_rollback();
+				$this->error("添加失败：".$this->_model->getError());
+			}else
+			{
+				//echo $this -> _model -> getLastSql();
+				$data[$this->_model->getPk()] = $id;
+				//log_db($this->_user['id'], $this->_model->getTableName(), 'insert', '', $data);				
+				$this->_after_add($data);
+				$this->assign('jumpUrl', __URL__. '/index');  //页面跳转
+				$this->success("添加成功!");
+			}
 		}
 		else 
 		{
+			
+			//商品品牌
+			$_brand_mod = D("GBrand");
+			$brand_list = $_brand_mod->select();
+			$this->assign('brand_list',$brand_list);
+			
 			$this->display();
 		}
 		
 	}
 	
+	
+	function _model()
+	{
+		return $this->_model = D('GGoods');
+	}
 }?> 
